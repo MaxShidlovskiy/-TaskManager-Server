9aeaee3952461d4e1cac8321b5138297768e2247
Maxim <shidloba@gmail.com>
2023-05-26 01:28:36 +0300

    create task.controller file

--- src/controller/task.controller.js
+++ src/controller/task.controller.js
@@ -1,24 +1,70 @@
-const express = require(`express`);
-const { getAllTask, createTask } = require(`../service/task.service`)
+const express = require('express');
+
+const { buildResponse } = require('../helper/buildResponse');
+const { getAllTask, getByIdTask, createTask, updateTask, deleteTask, patchTask } = require('../service/task.service')
+const { isvalidTaskId, isvalidTaskBody } = require('../helper/validtion');
+
 const route = express.Router();
 
 route.get('/', async (req, res) => {
     try {
         const data = await getAllTask();
-        res.send(data)
+        buildResponse(res, 200, data);
     } catch (error) {
-        res.send(error.message);
+        buildResponse(res, 404, error.message);
     }
 });
 
-route.post('/', async (req, res) => {
+route.get('/:id', isvalidTaskId, async (req, res) => {
+    try {
+        const { id } = req.params;
+        const data = await getByIdTask(id);
+        buildResponse(res, 200, data);
+    } catch (error) {
+        buildResponse(res, 404, error.message);
+    }
+});
+
+route.post('/', isvalidTaskBody, async (req, res) => {
     try {
         const { task, user_id } = req.body;
         const data = await createTask(task, user_id);
-        res.send(data)
+        buildResponse(res, 200, data);
     } catch (error) {
-        res.send(error.message)
+        buildResponse(res, 404, error.message);
     }
 });
 
-module.exports = route
+route.put('/:id', isvalidTaskId,isvalidTaskBody, async (req, res) => {
+    try {
+        const { id } = req.params;
+        const { task, user_id } = req.body;
+        const data = await updateTask(id, task, user_id);
+        buildResponse(res, 200, data);
+    } catch (error) {
+        buildResponse(res, 404, error.message);
+    }
+});
+
+route.delete('/:id', isvalidTaskId, async (req, res) => {
+    try {
+        const { id } = req.params;
+        const data = await deleteTask(id);
+        buildResponse(res, 200, data);
+    } catch (error) {
+        buildResponse(res, 404, error.message)
+    }
+});
+
+route.patch('/:id', isvalidTaskId, async (req, res) => {
+    try {
+        const { id } = req.params;
+        const clientData = req.body;
+        const data = await patchTask(id, clientData);
+        buildResponse(res, 200, data);
+    } catch (error) {
+        buildResponse(res, 404, error.message)
+    }
+})
+
+module.exports = route;
